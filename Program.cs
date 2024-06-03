297e6dcadfe5aa503858125f259f047039ea26ef
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-06-03 15:20:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment_5._2
+{
+    internal class Program
+    {
+        static int NumbersRecursive(int number)
+        {
+            if ( number != 0)
+            {
+                return number % 10 + NumbersRecursive(number/10);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            int result = NumbersRecursive(inputNumber);
+            Console.WriteLine($"The sum of the digits of the number {inputNumber} is: {result}");
+        }
+    }
+}
